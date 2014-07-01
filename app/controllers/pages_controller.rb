e821166fd6de18696938fcb2b3dacea4b69b36e3
Xaun Lopez <xaunlopez@gmail.com>
2014-07-01 12:31:46 +1000

    removed sort_by select inputs, and added tablesort to tracks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,36 +8,6 @@ class PagesController < ApplicationController
     # Determines which filter is selected and runs the search.
     if params[:filter] == '/tracks' && params[:search]
       @tracks = client.get(params[:filter], :q => params[:search], :limit => page_size)
-      # Checks for which sort_by option is selected within tracks filter, and sorts the results.
-      if params[:sort_by_title] == 'ascending'
-        @tracks.sort! { |a,b| a.title <=> b.title }
-      elsif params[:sort_by_title] == 'descending'
-        @tracks.sort! { |a,b| b.title <=> a.title }
-      elsif params[:sort_by_duration] == 'ascending'
-        @tracks.sort! { |a,b| a.duration <=> b.duration }
-      elsif params[:sort_by_duration] == 'descending'
-        @tracks.sort! { |a,b| b.duration <=> a.duration }
-      elsif params[:sort_by_playback_count] == 'ascending'
-        @tracks.sort! { |a,b| a.playback_count <=> b.playback_count }
-      elsif params[:sort_by_playback_count] == 'descending'
-        @tracks.sort! { |a,b| b.playback_count <=> a.playback_count }
-      elsif params[:sort_by_favoritings_count] == 'ascending'
-        @tracks.sort! { |a,b| a.favoritings_count <=> b.favoritings_count }
-      elsif params[:sort_by_favoritings_count] == 'descending'
-        @tracks.sort! { |a,b| b.favoritings_count <=> a.favoritings_count }
-      elsif params[:sort_by_created_at] == 'ascending'
-        @tracks.sort! { |a,b| a.created_at <=> b.created_at }
-      elsif params[:sort_by_created_at] == 'descending'
-        @tracks.sort! { |a,b| b.created_at <=> a.created_at }
-      elsif params[:sort_by_genre] == 'ascending'
-        @tracks.sort! { |a,b| a.genre <=> b.genre }
-      elsif params[:sort_by_genre] == 'descending'
-        @tracks.sort! { |a,b| b.genre <=> a.genre }
-      elsif params[:sort_by_bpm] == 'ascending'
-        @tracks.sort! { |a,b| a.bpm <=> b.bpm }
-      elsif params[:sort_by_bpm] == 'descending'
-        @tracks.sort! { |a,b| b.bpm <=> a.bpm }
-      end
     elsif params[:filter] == '/users' && params[:search]
       @users = client.get(params[:filter], :q => params[:search], :limit =>
         page_size)
@@ -46,6 +16,7 @@ class PagesController < ApplicationController
     else
       @tracks = []
       @users = []
+      @playlists = []
     end
   # raise 'test'
   end
