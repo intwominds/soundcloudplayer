c28332b603a753f8b10971c9ad0aaef452103beb
Xaun Lopez <xaunlopez@gmail.com>
2014-07-03 02:35:15 +1000

    dynamically displaying log in/signup and log off with messages + home icon link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,8 @@
 module ApplicationHelper
-  def smartnav
+  def smartindex
     links = ''
 
-    if @current_account.present?
-      links += "<div id='log-out'>"
-      links += link_to('Logout ' + @current_account.username, login_path, :data => {:method => :delete, :confirm => 'Really logout?'})
-      links += "</div>"
-    else
+    unless @current_account.present?
       links += "<div id='sign-up'>#{ link_to('Sign up', new_account_path) }</div>"
       links += "<div id='sign-in'>#{ link_to('Sign in', login_path) }</div>"
     end
