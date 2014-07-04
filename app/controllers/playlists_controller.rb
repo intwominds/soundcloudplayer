df8fb05532329d3957899e98dd42d64f02374723
Xaun Lopez <xaunlopez@gmail.com>
2014-07-04 12:00:31 +1000

    redirect to new_account_path if @account.nil

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,15 +1,19 @@
 class PlaylistsController < ApplicationController
 
   def add # add icons link to this action.
-    # finds the playlist that was assigned to your account when signed up (create function within accounts_controller), and assigns the playlist to @stash.
-    @stash = @current_account.playlists.first # Playlist.find(@current_account.id)
-    # creates a new track db entry from the track.id, which is interpolated and sent from the search.html.erb view via params[:id] & associates the playlist from @stash.id.
-    t = Track.create(
-      :soundcloud_track_id => params[:id],
-      :playlist_id => @stash.id
-    )
-    # raise 'test'
-    redirect_to(:back)
+    if @current_account.nil?
+      redirect_to new_account_path
+    else
+      # finds the playlist that was assigned to your account when signed up (create function within accounts_controller), and assigns the playlist to @stash.
+      @stash = @current_account.playlists.first # Playlist.find(@current_account.id)
+      # creates a new track db entry from the track.id, which is interpolated and sent from the search.html.erb view via params[:id] & associates the playlist from @stash.id.
+      t = Track.create(
+        :soundcloud_track_id => params[:id],
+        :playlist_id => @stash.id
+      )
+      # raise 'test'
+      redirect_to(:back)
+    end
   end
 
   def show
