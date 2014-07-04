44f120863a4b287166cc94f030d0e5a377d97d1e
Xaun Lopez <xaunlopez@gmail.com>
2014-07-04 10:26:03 +1000

    fixed show function in playlist_controller displaying random tracks.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,7 +2,7 @@ class PlaylistsController < ApplicationController
 
   def add # add icons link to this action.
     # finds the playlist that was assigned to your account when signed up (create function within accounts_controller), and assigns the playlist to @stash.
-    @stash = Playlist.find(@current_account.id)
+    @stash = @current_account.playlists.first # Playlist.find(@current_account.id)
     # creates a new track db entry from the track.id, which is interpolated and sent from the search.html.erb view via params[:id] & associates the playlist from @stash.id.
     t = Track.create(
       :soundcloud_track_id => params[:id],
@@ -17,8 +17,12 @@ class PlaylistsController < ApplicationController
     client = Soundcloud.new(:client_id => 'ad246bea735d1371bb0416e34ec114a1')
     page_size = 50 #set page size limiting search results.
     ids = @current_account.playlists.first.tracks.map { |t| t.soundcloud_track_id }
-    @tracks = client.get('/tracks?ids=' + ids.join(','))
-  render '/pages/search'
+    if ids.length > 0
+      @tracks = client.get('/tracks?ids=' + ids.join(','))
+    else
+      @tracks = []
+    end
+    render '/pages/search'
   end
 
   def remove
