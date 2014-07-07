337e1547272f7f83626d62cbb068f4b1b9e03efd
Xaun Lopez <xaunlopez@gmail.com>
2014-07-08 00:21:22 +1000

    changed seed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ Account.destroy_all
 Playlist.destroy_all
 Track.destroy_all
 
-u1 = Account.create(:username => 'xaun', :email => 'xaunlopez@gmail.com', :password => 'admin', :password_confirmation => 'admin', :is_admin => true)
+u1 = Account.create(:username => 'zim', :password => 'doom', :password_confirmation => 'doom', :is_admin => true)
